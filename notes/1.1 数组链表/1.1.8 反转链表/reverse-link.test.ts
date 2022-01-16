b3541f0ec68d2b99e7e3edc73d47698d36cf1e5c
hekunyu <hekunyu@didiglobal.com>
2022-01-16 15:24:56 +0800

    fix: 调整反转链表用例

--- notes/1.1 数组链表/1.1.8 反转链表/reverse-link.test.ts
+++ notes/1.1 数组链表/1.1.8 反转链表/reverse-link.test.ts
@@ -1,17 +1,17 @@
 import { arrToLink, reverseLink, reverseLinkIt} from './reverse-link'
 
-const arr = [1,2,3,4,5]
 describe('反转链表', () => {
   it('边界', () => {
-    const t = arrToLink([1])
-    console.log(t)
+    arrToLink([1])
   })
   it('递归', () => {
+    const arr = [1,2,3,4,5]
     const t = arrToLink(arr)
     expect(reverseLink(t)).toEqual(arrToLink(arr.reverse()))
   })
   
   it('迭代', () => {
+    const arr = [1,2,3,4,5]
     const t = arrToLink([1,2,3,4,5])
     expect(reverseLinkIt(t)).toEqual(arrToLink(arr.reverse()))
   })
